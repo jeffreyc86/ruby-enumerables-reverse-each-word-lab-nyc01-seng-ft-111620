88785ffde37ed7675de3da7dd5fe63339c4bdab1
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-03 18:17:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word (str)
   arr = str.split (" ")
   new_arr = []
   arr.each do |s|
-    new_arr = new_arrs.reverse.join (" ")
+    new_arr = new_arr.pushs.reverse.join (" ")
   end
   new_str
 end
